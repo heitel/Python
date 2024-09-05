03d0e5a69455ed0735fcc339751303ae4d18eb22
heitel <joachim@heitel.name>
2024-09-05 17:27:28 +0200

    V21

--- ScanNetwork.py
+++ ScanNetwork.py
@@ -1,5 +1,4 @@
 import logging
-
 import netifaces
 
 logging.getLogger("scapy").setLevel(logging.CRITICAL)
@@ -9,7 +8,7 @@ from scapy.layers.inet6 import *
 from scapy.layers.l2 import ARP, arping
 
 
-def reverse_dns_lookup(ip_address):
+def reverse_dns_lookup(ip_address: str):
     try:
         # Führt eine Rückwärts-DNS-Suche durch und gibt den Hostnamen zurück
         host_name, _, _ = socket.gethostbyaddr(ip_address)
@@ -17,50 +16,37 @@ def reverse_dns_lookup(ip_address):
     except socket.herror:
         return None
 
-def scan_network(network_range):
-    t = AsyncSniffer(filter="arp and arp[7]==2", timeout=10)
+
+def scan_network(network_range: str):
+    # Filter for ARP-Reply
+    t = AsyncSniffer(filter="arp and arp[7]==2", timeout=5)
     t.start()
-    ans = arping("10.192.0.0/19", cache=False, verbose=1, timeout=10)[0]
-    t.join()
-
-    devices = []
-    for p in t.results:
-        # print(f"{p.psrc, p.hwsrc}")
-        devices.append({'ip': p.psrc, 'mac': p.hwsrc})
-
-    print(f"Gefunden: {len(devices)} IPv4 Devices", flush=True)
-    return devices
-
-def scan_network2(network_range):
+    #    arping(network_range, verbose=False, timeout=5)
     arp_request = ARP(pdst=network_range)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether / arp_request
-    result = srp(packet, timeout=10, verbose=0)[0]
+    srp(packet, timeout=5, verbose=0)
+    t.join()
 
-    devices = []
-    # Verarbeitung der erhaltenen Antworten
-    for sent, received in result:
-        devices.append({'ip': received.psrc, 'mac': received.hwsrc})
-
+    devices = dict()
+    for p in t.results:
+        devices[p[Ether].src] = p[ARP].psrc
     print(f"Gefunden: {len(devices)} IPv4 Devices", flush=True)
     return devices
 
-def sniff_icmpv6_responses(iface, timeout=10):
-    # Sniff IPv6-Pakete auf dem Interface
-    # ICMPv6 Request and Reply
-    packets = sniff(filter="icmp6 and (ip6[40] == 128 or ip6[40] == 129)", iface=iface, timeout=timeout, promisc=True)
-
-    erg = dict()
-    for packet in packets:
-        erg[packet[Ether].src] = packet[IPv6].src
-            #print(f"Antwort von: {packet[IPv6].src} {packet[Ether].src}")
-
-    return erg
 
 def send_icmpv6_multicast(iface):
-    for i in range(1):
-        icmpv6_request = IPv6(dst="ff02::1") / ICMPv6EchoRequest(seq=i)
-        send(icmpv6_request, iface=iface, verbose=False)
+    t = AsyncSniffer(filter="icmp6 and (ip6[40] == 128 or ip6[40] == 129)", iface=iface, timeout=10, promisc=True)
+    t.start()
+    icmpv6_request = IPv6(dst="ff02::1") / ICMPv6EchoRequest()
+    send(icmpv6_request, iface=iface, verbose=False)
+    t.join()
+
+    erg = dict()
+    for packet in t.results:
+        erg[packet[Ether].src] = packet[IPv6].src
+
+    return erg
 
 
 if __name__ == "__main__":
@@ -68,34 +54,26 @@ if __name__ == "__main__":
     for iface in netifaces.interfaces():
         addrs = netifaces.ifaddresses(iface)
         for addr in addrs.get(netifaces.AF_INET, []):
-            print('%-8s %s' % ( iface, addr['addr']))
+            print('%-8s %s' % (iface, addr['addr']))
 
     print("IPv6 Interfaces *****************************")
     for iface in netifaces.interfaces():
         addrs = netifaces.ifaddresses(iface)
         for addr in addrs.get(netifaces.AF_INET6, []):
             print('%-8s %s' % (iface, addr['addr']))
-    print("Welches Interface soll gescannt werden? ", end="")
- #   interface = input()
- #   if interface == "":
- #       interface = "enp0s1"
+    #   print("Welches Interface soll gescannt werden? ", end="")
+    #   interface = input()
+    #   if interface == "":
+    #       interface = "enp0s1"
 
     conf.use_pcap = True
     interface = conf.iface
-   # interface = "Ethernet"
+    # interface = "Ethernet"
     print(f"Start scanning {interface}..................", flush=True)
-    # Netzwerkrange, die gescannt werden soll, z.B. "192.168.0.0/24"
-    #network_range = "192.168.0.0/24"
-    network_range = "10.192.0.0/19"
+    network_range = "192.168.0.0/24"
+    #network_range = "10.192.0.0/19"
     devices = scan_network(network_range)
-
-    # Netzwerk-Interface, über das das Paket gesendet wird
-    # win
-    # interface = "WLAN"
-    # macos
-    # interface = "enp0s1"
-    send_icmpv6_multicast(interface)
-    erg = sniff_icmpv6_responses(interface)
+    erg = send_icmpv6_multicast(interface)
 
     # Ausgabe der gefundenen Geräte
     print("Gefundene Geräte im Netzwerk:")
@@ -104,12 +82,10 @@ if __name__ == "__main__":
 
     i = 1
     for device in devices:
-        hostname = reverse_dns_lookup(device['ip'])
+        hostname = reverse_dns_lookup(devices[device])
         try:
-            ipv6 = erg[device['mac']]
+            ipv6 = erg[device]
         except:
             ipv6 = "n./a."
-        print(f"{i}. {device['mac']}  {device['ip']: >16}    {ipv6: >25}    {hostname}")
+        print(f"{i}. {device}  {devices[device]: >16}    {ipv6: >25}    {hostname}")
         i += 1
-
-
